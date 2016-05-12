1eff159e9aa17941e0bc527028c61cd2d0cd1b8d
Vladimir Panteleev <git@thecybershadow.net>
2016-05-12 01:31:40 +0000

    tests: More args

--- tests/args/src.result/test.d
+++ tests/args/src.result/test.d
@@ -16,11 +16,30 @@ void c(){
 	total++;
 }
 
+void d1(int foo)
+{
+	total += foo;
+}
+
+void d2(int , int bar)
+{
+	total += bar;
+}
+
+void d3(int , int , int baz)
+{
+	total += baz;
+}
+
 void main()
 {
 	a;
 	b!()(3);
 	c;
 
-	writeln(total==3);
+	d1(1);
+	d2(0, 1);
+	d3(0, 0, 1);
+
+	writeln(total==6);
 }
--- tests/args/src/test.d
+++ tests/args/src/test.d
@@ -17,11 +17,30 @@ void c(int foo, int bar, int baz)()
 	total++;
 }
 
+void d1(int foo, int bar, int baz)
+{
+	total += foo;
+}
+
+void d2(int foo, int bar, int baz)
+{
+	total += bar;
+}
+
+void d3(int foo, int bar, int baz)
+{
+	total += baz;
+}
+
 void main()
 {
 	a(1, 2, 3);
 	b!(1, 2)(3);
 	c!(1, 2, 3);
 
-	writeln(total==3);
+	d1(1, 0, 0);
+	d2(0, 1, 0);
+	d3(0, 0, 1);
+
+	writeln(total==6);
 }
