a1eae65b73f6892d6c12c6abc7090bad32dec79f
Vladimir Panteleev <git@thecybershadow.net>
2020-03-17 19:21:48 +0000

    tests/syntax: Uncomment D1 naked escape string test

--- tests/syntax/src/syntax.d
+++ tests/syntax/src/syntax.d
@@ -3,7 +3,7 @@ void main()
 	char c1 = 'a';
 	char c2 = '\n';
 	string s1 = "a\n\x01\u0123\U00012345";
-	// string s2 = \n; // D1 naked escape string
+	string s2 = \n; // D1 naked escape string
 	string s3 = r"abc\def";
 	string s4 = `ab\cd"ef`;
 	/* comment 1 */
